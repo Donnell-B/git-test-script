43fc7e782b54858bb99b722610baf75d879385ea
Donnell <trainee.donnell.burgess@sigmalabs.co.uk>
2025-03-11 10:47:34 +0000

    Test

--- commit.sh
+++ commit.sh
@@ -2,12 +2,16 @@ function commit() {
     files=$({ git diff --name-only ; git diff --name-only --staged ; })
     for file in $files
     do
-        echo $file
+        git add $file
     done
+    read -p "Please add a commit message: " commit_msg
+    echo $commit_msg
+    git commit -m $commit_msg
+    git push
 }
 
 commit
-
+#something here
 # git diff --name-only --cached
 
 # function hello() {
