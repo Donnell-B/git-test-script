87d4cb6882daf3382ab3e5f64cd197d3be9c9838
Donnell <trainee.donnell.burgess@sigmalabs.co.uk>
2025-03-11 11:43:07 +0000

    Added Extra Checks

--- commit.sh
+++ commit.sh
@@ -1,6 +1,29 @@
 function commit() {
+    git_status=$( git status )
+    if [[ "$git_status" == *"fatal: not a git repository"* ]]; then
+        echo "Not in a Git Repository! Exiting"
+        exit 0
+    fi
+
     git fetch
     git pull || echo "Git Pull failed -- Please Pull Manually"
+
+    tracked_files=$( git ls-files -z )
+    if [[ -z $tracked_files ]]; then
+        echo "You have no tracked files!"
+        echo "Add all files (.) now? (Y/N)"
+        read choice
+        if [[ $choice == 'Y' || $choice == 'y' ]]; then
+            git add .
+            git commit -m "Tracking initial files"
+            git push
+            echo "Files added, committed, and pushed successfully."
+            exit 1
+        else
+            echo "No files added. Exiting."
+            exit 0
+        fi
+    fi
 
     files=$( { git diff --name-only; git diff --name-only --staged; } | sort | uniq )
 
@@ -12,7 +35,7 @@ function commit() {
     echo "Changed Detected..."
     for file in $files
     do
-        echo "Adding $file"
+        echo "Staged: $file"
         git add $file
     done
     read -p "Please add a commit message: " commit_msg
